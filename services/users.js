58b8c136e698e409fe3ef906fd0493157085e322
oscar02r <oscary.ramirez@gmail.com>
2020-02-02 12:26:48 +0100

    Modifyded user Schema

--- services/users.js
+++ services/users.js
@@ -25,6 +25,19 @@ class UsersService{
 
           return createUserId;
       }
+
+      async getOrCreateUser({user}){
+         const queriedUser = await this.getUser({email:user.email});
+
+         if (queriedUser) {
+             return queriedUser
+         }
+
+         await this.createUser({user});
+         
+         return  await this.getUser({email:user.email});
+
+      }
 }
 
 module.exports = UsersService;
--- utils/schemas/user.js
+++ utils/schemas/user.js
@@ -2,14 +2,24 @@ const joi = require('@hapi/joi');
 
 const userIdSchema = joi.object({id:joi.string().regex(/^[0-9a-fA-F]{24}$/)});
 
-const createUserSchema = joi.object({
+const userSchema = joi.object({
     name:joi.string().max(100).required(),
     email: joi.string().email().required(),
     password:joi.string().required(),
-    isAdmin: joi.boolean()
 });
+
+const createUserSchema = {
+      ...userSchema,
+      isAdmin: joi.boolean()
+};
+
+const createProviderUserSchema = {
+    ...userSchema,
+    apiKeyToken:joi.string().required()
+};
 
 module.exports = {
     userIdSchema,
-    createUserSchema
+    createUserSchema,
+    createProviderUserSchema
 };
