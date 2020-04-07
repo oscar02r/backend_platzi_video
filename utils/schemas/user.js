325de9649219f69eb44afbdb1d10aeeaee3e3e7c
oscar02r <oscary.ramirez@gmail.com>
2020-04-08 00:00:41 +0200

    Fixe schema user

--- utils/schemas/user.js
+++ utils/schemas/user.js
@@ -1,17 +1,19 @@
 const joi = require('@hapi/joi');
 
-const userIdSchema = joi.object({id:joi.string().regex(/^[0-9a-fA-F]{24}$/)});
+const userIdSchema = joi.object({ id: joi.string().regex(/^[0-9a-fA-F]{24}$/)});
 
-const userSchema = joi.object({
+/*const userSchema = joi.object({
     name:joi.string().max(100).required(),
     email: joi.string().email().required(),
     password:joi.string().required(),
-});
+});*/
 
-const createUserSchema = {
-      ...userSchema,
+const createUserSchema = joi.object( {
+      name:joi.string().max(100).required(),
+      email: joi.string().email().required(),
+      password:joi.string().required(),
       isAdmin: joi.boolean()
-};
+});
 
 const createProviderUserSchema = joi.object({
     
